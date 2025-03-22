6f395183e83acca75dcf311ceb8e4182b21154ac
paxmix <manh1998nb@gmail.com>
2025-03-22 21:34:29 +0700

    added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.color_scheme = "Tokyo Night Storm"
+config.font = wezterm.font("FantasqueSansM Nerd Font")
+config.font_size = 13
+
+config.default_cursor_style = "SteadyBar"
+
+config.alternate_buffer_wheel_scroll_speed = 5
+
+config.hide_mouse_cursor_when_typing = true
+
+config.initial_cols = 180
+config.initial_rows = 45
+
+config.max_fps = 75
+
+config.use_fancy_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
