051e146e51a2c3611009eeb82c128224fed068da
paxmix <manh1998nb@gmail.com>
2025-03-25 11:04:48 +0700

    update tab bar config wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -27,8 +27,47 @@ config.initial_rows = 45
 -- set max_fps to 75, probably not doing anything
 config.max_fps = 75
 
-config.use_fancy_tab_bar = false
+-- config.use_fancy_tab_bar = false
+config.window_frame = {
+	font = wezterm.font({ family = "FantasqueSansM Nerd Font", weight = "Bold" }),
 
+	font_size = 13.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#1e1e2e",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#1e1e2e",
+}
+
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			bg_color = "#353749",
+			fg_color = "#cdd6f4",
+		},
+		inactive_tab = {
+			bg_color = "#1e1e2e",
+			fg_color = "#cdd6f4",
+		},
+		inactive_tab_hover = {
+			bg_color = "#242638",
+			fg_color = "#cdd6f4",
+			italic = true,
+		},
+		new_tab = {
+			bg_color = "#1e1e2e",
+			fg_color = "#cdd6f4",
+		},
+		new_tab_hover = {
+			bg_color = "#242638",
+			fg_color = "#cdd6f4",
+			italic = true,
+		},
+	},
+}
 -- config.window_background_opacity = 0.8
 
 -- and finally, return the configuration to wezterm
