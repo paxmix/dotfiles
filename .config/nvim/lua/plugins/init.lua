bbac3742d6f329c6b03ca00462e4c568db538035
paxmix <manh1998nb@gmail.com>
2025-03-28 23:35:17 +0700

    updated lualine

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -66,12 +66,9 @@ if true then
     {
       "nvim-lualine/lualine.nvim",
       event = "VeryLazy",
-      opts = function(_, opts)
-        table.insert(opts.sections.lualine_x, {
-          "lsp_status",
-          icon = "󰒋", -- f013
-        })
-      end,
+      opts = {
+        sections = { lualine_z = { { "lsp_status", icon = "" } } },
+      },
     },
   }
 end
