d71a19a5d91aabdb25efac95faddf06e4f1b6523
paxmix <manh1998nb@gmail.com>
2025-04-10 05:15:55 +0700

    update fish abbr for openSUSE

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,36 +17,12 @@ end
 zoxide init --cmd cd fish | source
 
 # eza
-abbr ls "eza -a --icons=auto --color=always"
-abbr ll "eza -la --icons=auto --color=always"
-abbr lt "eza -Ta --icons=auto --color=always"
+alias ls "eza -a --icons=auto --color=always"
+alias ll "eza -la --icons=auto --color=always"
+alias lt "eza -Ta --icons=auto --color=always"
 
 # Set up fzf key bindings
 fzf --fish | source
-# fzf tokyo night theme
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
-  --highlight-line \
-  --info=inline-right \
-  --ansi \
-  --layout=reverse \
-  --border=none \
-  --color=bg+:#2e3c64 \
-  --color=bg:#1f2335 \
-  --color=border:#29a4bd \
-  --color=fg:#c0caf5 \
-  --color=gutter:#1f2335 \
-  --color=header:#ff9e64 \
-  --color=hl+:#2ac3de \
-  --color=hl:#2ac3de \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#2ac3de \
-  --color=query:#c0caf5:regular \
-  --color=scrollbar:#29a4bd \
-  --color=separator:#ff9e64 \
-  --color=spinner:#ff007c \
-"
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
@@ -72,7 +48,7 @@ set -g hydro_color_git cba6f7
 set -g hydro_color_prompt a6e3a1
 set -g hydro_color_error f38ba8
 set -g hydro_color_duration f9e2af
-set -g hydro_cmd_duration_threshold 5000
+set -g hydro_cmd_duration_threshold 100
 set -g fish_prompt_pwd_dir_length 0
 # set -g hydro_multiline true
 
@@ -100,16 +76,7 @@ if test -d ~/.cargo/bin
     end
 end
 
-# Add depot_tools to PATH
-if test -d ~/Applications/depot_tools
-    if not contains -- ~/Applications/depot_tools $PATH
-        set -p PATH ~/Applications/depot_tools
-    end
-end
-
 # Custom abbreviation
 abbr lg lazygit
-abbr mirror "sudo cachyos-rate-mirrors"
-abbr update 'sudo pacman -Syu'
-abbr cleanup 'sudo pacman -Rns (pacman -Qtdq)'
-abbr logout 'sudo systemctl restart ly'
+abbr update 'sudo zypper update'
+abbr cleanup 'sudo zypper remove -u'
