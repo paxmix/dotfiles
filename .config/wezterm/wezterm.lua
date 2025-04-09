8eebfde7cac60576d8a44b482f3fec073ef8b5df
paxmix <manh1998nb@gmail.com>
2025-04-10 06:38:22 +0700

    update wezterm theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ config.default_prog = { "/usr/bin/fish", "-l" }
 
 -- config.color_scheme = "Tokyo Night Storm"
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Dracula (Official)"
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12
@@ -27,47 +27,10 @@ config.initial_rows = 42
 -- set max_fps to 75, probably not doing anything
 config.max_fps = 75
 
--- config.use_fancy_tab_bar = false
-config.window_frame = {
-	font = wezterm.font({ family = "JetBrainsMono Nerd Font", weight = "Bold" }),
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 
-	font_size = 12.0,
-
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#1e1e2e",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#1e1e2e",
-}
-
-config.colors = {
-	tab_bar = {
-		active_tab = {
-			bg_color = "#353749",
-			fg_color = "#cdd6f4",
-		},
-		inactive_tab = {
-			bg_color = "#1e1e2e",
-			fg_color = "#cdd6f4",
-		},
-		inactive_tab_hover = {
-			bg_color = "#242638",
-			fg_color = "#cdd6f4",
-			italic = true,
-		},
-		new_tab = {
-			bg_color = "#1e1e2e",
-			fg_color = "#cdd6f4",
-		},
-		new_tab_hover = {
-			bg_color = "#242638",
-			fg_color = "#cdd6f4",
-			italic = true,
-		},
-	},
-}
 -- config.window_background_opacity = 0.8
 
 -- and finally, return the configuration to wezterm
