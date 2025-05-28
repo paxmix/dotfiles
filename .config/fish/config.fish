5b143fe647a32ee294502649368e75d6214df24e
paxmix <manh1998nb@gmail.com>
2025-05-28 13:47:42 +0700

    replace starship for hydro

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,6 +24,13 @@ if test -f ~/.fish_profile
 end
 
 # Add ~/.local/bin to PATH
+if test -d /opt/nvim
+    if not contains -- /opt/nvim $PATH
+        set -p PATH /opt/nvim
+    end
+end
+
+# Add ~/.local/bin to PATH
 if test -d ~/.local/bin
     if not contains -- ~/.local/bin $PATH
         set -p PATH ~/.local/bin
@@ -59,8 +66,30 @@ alias l. "eza -a | grep -e '^\.'" # list only dotfiles
 
 # Set up fzf key bindings
 # need fzf version > 0.48
-fzf --fish | source
-
+# fzf --fish | source
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
@@ -76,17 +105,53 @@ export FZF_ALT_C_OPTS="
   --walker-skip .git,node_modules,target
   --preview 'tree -C {}'"
 
-function starship_transient_prompt_func
-    tput cuu1
-    starship module character
-end
+# Fish Tokyo Night colorscheme
+# TokyoNight Color Palette
+set -l foreground c0caf5
+set -l selection 283457
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
 
-function prompt_newline --on-event fish_postexec
-    echo
-end
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_option $pink
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
 
-alias clear "command clear; commandline -f clear-screen"
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+set -g fish_pager_color_selected_background --background=$selection
 
-# Starship
-starship init fish | source
-enable_transience
+# Hydro config
+set -g hydro_symbol_start " "
+set -g hydro_symbol_prompt ➜
+
+set -g hydro_color_pwd $cyan
+set -g hydro_color_git $purple
+set -g hydro_color_start $green
+set -g hydro_color_error $red
+set -g hydro_color_prompt $green
+set -g hydro_color_duration $yellow
+
+set -g fish_prompt_pwd_dir_length 4
+set -g hydro_cmd_duration_threshold 5000
