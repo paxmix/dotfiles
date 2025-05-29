2237530a3a4750d48875629bc295d840769d5629
paxmix <manh1998nb@gmail.com>
2025-05-29 08:24:41 +0700

    update fish config for cachyos

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,7 +53,7 @@ end
 
 # Custom abbreviation
 abbr lg lazygit
-abbr update 'sudo pacman -Syu && flatpak update'
+abbr cleanup "sudo pacman -Rns (pacman -Qtdq)"
 
 # Zoxide
 zoxide init --cmd cd fish | source
@@ -66,32 +66,32 @@ alias l. "eza -a | grep -e '^\.'" # list only dotfiles
 
 # Set up fzf key bindings
 # need fzf version > 0.48
-# fzf --fish | source
+fzf --fish | source
 
 # fzf TokyoNight colorscheme
-# export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
-#   --highlight-line \
-#   --info=inline-right \
-#   --ansi \
-#   --layout=reverse \
-#   --border=none \
-#   --color=bg+:#283457 \
-#   --color=bg:#16161e \
-#   --color=border:#27a1b9 \
-#   --color=fg:#c0caf5 \
-#   --color=gutter:#16161e \
-#   --color=header:#ff9e64 \
-#   --color=hl+:#2ac3de \
-#   --color=hl:#2ac3de \
-#   --color=info:#545c7e \
-#   --color=marker:#ff007c \
-#   --color=pointer:#ff007c \
-#   --color=prompt:#2ac3de \
-#   --color=query:#c0caf5:regular \
-#   --color=scrollbar:#27a1b9 \
-#   --color=separator:#ff9e64 \
-#   --color=spinner:#ff007c \
-# "
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
@@ -145,14 +145,14 @@ set -g fish_pager_color_description $comment
 set -g fish_pager_color_selected_background --background=$selection
 
 # Hydro config
-set -g hydro_symbol_start " "
+set -g hydro_symbol_start " "
 set -g hydro_symbol_prompt ➜
 
 set -g hydro_color_pwd $cyan
 set -g hydro_color_git $purple
 set -g hydro_color_start $green
 set -g hydro_color_error $red
-set -g hydro_color_prompt $green
+set -g hydro_color_prompt $cyan
 set -g hydro_color_duration $yellow
 
 set -g fish_prompt_pwd_dir_length 4
