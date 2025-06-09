314e2e2452a827f3344f7d8f982a980281af7525
paxmix <manh1998nb@gmail.com>
2025-06-09 16:40:56 +0700

    cleanup config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -61,7 +61,7 @@ alias l. "eza -a | grep -e '^\.'" # list only dotfiles
 # need fzf version > 0.48
 fzf --fish | source
 
-set -g FZF_DEFAULT_OPTS "\
+set -Ux FZF_DEFAULT_OPTS "\
 --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
 --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
 --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
@@ -88,6 +88,7 @@ function starship_transient_prompt_func
 end
 starship init fish | source
 enable_transience
+
 function prompt_newline --on-event fish_postexec
     echo
 end
