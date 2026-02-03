066c8f7ecf8c5f377138e528774aebe0393c92c3
paxmix <manh1998nb@gmail.com>
2026-02-03 22:39:41 +0700

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,20 +33,6 @@ if test -d ~/.local/bin
     end
 end
 
-# Add .cargo/bin to PATH
-if test -d ~/.cargo/bin
-    if not contains -- ~/.cargo/bin $PATH
-        set -p PATH ~/.cargo/bin
-    end
-end
-
-# Add ~/go/bin to PATH
-if test -d ~/go/bin
-    if not contains -- ~/go/bin $PATH
-        set -p PATH ~/go/bin
-    end
-end
-
 # Custom abbreviation
 abbr lg lazygit
 abbr cleanup "sudo pacman -Rns (pacman -Qtdq)"
@@ -63,42 +49,6 @@ alias l. "eza -a | grep -e '^\.'" # list only dotfiles
 # Set up fzf key bindings
 # need fzf version > 0.48
 fzf --fish | source
-
-# TokyoNight Color Palette
-set -l foreground c0caf5
-set -l selection 283457
-set -l comment 565f89
-set -l red f7768e
-set -l orange ff9e64
-set -l yellow e0af68
-set -l green 9ece6a
-set -l purple 9d7cd8
-set -l cyan 7dcfff
-set -l pink bb9af7
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_option $pink
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-set -g fish_pager_color_selected_background --background=$selection
 
 # fzf theme for Fish
 set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS \
@@ -139,53 +89,6 @@ export FZF_CTRL_R_OPTS="
 export FZF_ALT_C_OPTS="
   --walker-skip .git,node_modules,target
   --preview 'tree -C {}'"
-
-# One Dark Darker theme for Fish shell
-# Syntax Highlighting Colors
-set -g fish_color_normal abb2bf
-set -g fish_color_command 61afef
-set -g fish_color_keyword c678dd
-set -g fish_color_quote e5c07b
-set -g fish_color_redirection 56b6c2
-set -g fish_color_end 98c379
-set -g fish_color_error e06c75
-set -g fish_color_param abb2bf
-set -g fish_color_comment 5c6370 --italics
-set -g fish_color_selection --background=2c323c
-set -g fish_color_search_match --background=2c323c
-set -g fish_color_operator 56b6c2
-set -g fish_color_escape c678dd
-set -g fish_color_autosuggestion 5c6370
-set -g fish_color_cancel e06c75
-set -g fish_color_cwd 98c379
-set -g fish_color_user 56b6c2
-set -g fish_color_host 61afef
-set -g fish_color_host_remote e5c07b
-set -g fish_color_status e06c75
-
-# Completion Pager Colors
-set -g fish_pager_color_progress 5c6370
-set -g fish_pager_color_prefix 61afef --bold
-set -g fish_pager_color_completion abb2bf
-set -g fish_pager_color_description 5c6370
-set -g fish_pager_color_selected_background --background=2c323c
-set -g fish_pager_color_selected_prefix 61afef --bold
-set -g fish_pager_color_selected_completion abb2bf
-set -g fish_pager_color_selected_description 5c6370
-set -g fish_pager_color_secondary_background
-set -g fish_pager_color_secondary_prefix 61afef
-set -g fish_pager_color_secondary_completion abb2bf
-set -g fish_pager_color_secondary_description 5c6370
-
-# Git prompt colors (if using fish_git_prompt)
-set -g __fish_git_prompt_color_branch c678dd
-set -g __fish_git_prompt_color_branch_detached e06c75
-set -g __fish_git_prompt_color_upstream 56b6c2
-set -g __fish_git_prompt_color_dirtystate e5c07b
-set -g __fish_git_prompt_color_stagedstate 98c379
-set -g __fish_git_prompt_color_invalidstate e06c75
-set -g __fish_git_prompt_color_untrackedfiles e5c07b
-set -g __fish_git_prompt_color_cleanstate 98c379
 
 function starship_transient_prompt_func
     starship module character
