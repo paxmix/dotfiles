4aa0183d03c29bd8a51d41ee6b4dc8c36a9bbece
paxmix <manh1998nb@gmail.com>
2026-05-22 23:41:06 +0700

    change to tokyonight

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,12 +27,12 @@ if test -f ~/.fish_profile
 end
 # add paths
 fish_add_path ~/.local/bin
+fish_add_path ~/go/bin
 
 # Custom abbreviation / aliases
 alias lg lazygit
 abbr cleanup "sudo pacman -Rns (pacman -Qtdq)"
 alias zed zeditor
-alias vi nvim
 
 # Zoxide
 zoxide init --cmd cd fish | source
@@ -46,13 +46,29 @@ alias l. "eza -a | grep -e '^\.'" # list only dotfiles
 # Set up fzf key bindings, need fzf version > 0.48
 fzf --fish | source
 
-# fzf theme for Fish
-set -x FZF_DEFAULT_OPTS "\
---color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
---color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
---color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8 \
---color=selected-bg:#45475A \
---color=border:#6C7086,label:#CDD6F4"
+# fzf theme (Tokyo Night)
+set -x FZF_DEFAULT_OPTS " \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c"
 
 # Preview file content using bat (https://github.com/sharkdp/bat)
 set -x FZF_CTRL_T_OPTS "
