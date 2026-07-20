1f5e67963bdb9a694b5095490efe74a3b5d85a3a
paxmix <manh1998nb@gmail.com>
2026-07-20 11:53:16 +0700

    add go support

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -3,6 +3,7 @@ local options = {
     lua = { "stylua" },
     -- css = { "prettier" },
     -- html = { "prettier" },
+    go = { "goimports", "gofumpt" },
   },
 
   format_on_save = {
--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "marksman" }
+local servers = { "html", "cssls", "marksman", "gopls", "golangci_lint_ls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
