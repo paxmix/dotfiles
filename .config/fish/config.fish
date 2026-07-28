d840110cc38bbc460f3737d22f73173c61cd6b3c
paxmix <manh1998nb@gmail.com>
2026-07-29 00:02:59 +0700

    remove doom one theme for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,38 +26,6 @@ fzf --fish | source
 
 mise activate fish | source
 
-# Doom One Theme
-set -l foreground bbc2cf normal
-set -l selection 3f444a brcyan
-set -l comment 5b6268 brblack
-set -l red ff6c6b red
-set -l orange da8548 brred
-set -l yellow ECBE7B yellow
-set -l green 98be65 green
-set -l purple c678dd magenta
-set -l cyan 46d9ff cyan
-set -l pink a9a1e1 brmagenta
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $pink
-set -g fish_color_autosuggestion $comment
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
-
 function starship_transient_prompt_func
     starship module character
 end
