7a406c8111a88f9eb717ab594be503d33e938705
paxmix <manh1998nb@gmail.com>
2026-07-30 13:28:50 +0700

    update nvim config

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -3,7 +3,6 @@ local options = {
     lua = { "stylua" },
     -- css = { "prettier" },
     -- html = { "prettier" },
-    go = { "goimports", "gofumpt" },
   },
 
   format_on_save = {
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,14 +1,5 @@
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    build = "npm install",
-  },
-  {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
     ft = { "markdown" },
